f292ab2edd5c005411b155625c45968a6b8c3bfe
Mr`C <635452885@qq.com>
2019-12-20 22:36:59 +0800

    add some helper

---
+++ CCmall.Common/Helper/JwtHelper.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Linq;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CCmall.Common.Helper
+{
+    public class JwtHelper
+    {
+        /// <summary>
+        /// 颁发Jwt证书
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public static string IssueJwt(JwtTokenModel token)
+        {
+            //init
+            var issure = Appsettings.app(new string[] { "Audience", "Issure" });
+            var audince = Appsettings.app(new string[] { "Audience", "Audience" });
+            var secret = Appsettings.app(new string[] { "Audience", "Secret" });
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Jti, token.Uid.ToStr()),
+                new Claim(JwtRegisteredClaimNames.Iss, issure),
+                new Claim(JwtRegisteredClaimNames.Aud, audince),
+                new Claim(JwtRegisteredClaimNames.Iat, $"{new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds()}"),
+                new Claim(JwtRegisteredClaimNames.Nbf, $"{new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds()}") ,
+                new Claim(JwtRegisteredClaimNames.Exp, $"{new DateTimeOffset(DateTime.Now.AddSeconds(1000)).ToUnixTimeSeconds()}"),//过期时间
+            };
+            claims.AddRange(token.Role.Split(',').Select(s => new Claim(ClaimTypes.Role, s)));
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            //issure
+            var jwt = new JwtSecurityToken(issure, audince, claims, signingCredentials: creds);
+            var handler = new JwtSecurityTokenHandler();
+            var ret = handler.WriteToken(jwt);
+            return ret;
+        }
+        /// <summary>
+        /// 解析Jwt
+        /// </summary>
+        /// <param name="jwt"></param>
+        /// <returns></returns>
+        public static JwtTokenModel ParsingJwt(string jwt)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var ret = new JwtTokenModel();
+            var token = handler.ReadJwtToken(jwt);
+            try
+            {
+                token.Payload.TryGetValue(ClaimTypes.Role, out var roles);
+                if (roles == null)
+                {
+                    return ret;
+                }
+                ret.Role = roles.ToStr()
+                                .Replace("\n", "")
+                                .Replace(" ", "")
+                                .Replace("\t", "")
+                                .Replace("\r", "")
+                                .Replace("\"", "")
+                                .Trim('[', ']');
+                ret.Uid = token.Id.ToInt();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            return ret;
+        }
+    }
+
+    public class JwtTokenModel
+    {
+        public long Uid { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ CCmall.Common/Helper/ObjHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CCmall.Common.Helper
+{
+    public static class ObjHelper
+    {
+        public static int ToInt(this object obj)
+        {
+            var ret = 0;
+            if (obj == null)
+            {
+                return ret;
+            }
+            if (obj != null && obj != DBNull.Value && int.TryParse(obj.ToString(), out ret))
+            {
+                return ret;
+            }
+            return ret;
+        }
+        public static string ToStr(this object obj)
+        {
+            var ret = string.Empty;
+            if (obj == null)
+            {
+                return ret;
+            }
+            if (obj != null && obj != DBNull.Value)
+            {
+                return obj.ToString();
+            }
+            return ret;
+        }
+        public static bool ToBool(this object obj)
+        {
+            var ret = false;
+            if (obj == null)
+            {
+                return ret;
+            }
+            if (obj != null && obj != DBNull.Value && bool.TryParse(obj.ToString(), out ret))
+            {
+                return ret;
+            }
+            return ret;
+        }
+    }
+}
