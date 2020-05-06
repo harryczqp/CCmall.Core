337d1875d215f8495a5dbcd08979856abd61d8a9
Mr`C <zhengqin.cao@siger-data.com>
2020-05-06 18:14:01 +0800

    update

--- CCmall.Repository/Interface/IRouterRepository.cs
+++ CCmall.Repository/Interface/IRouterRepository.cs
@@ -1,7 +1,11 @@
 ﻿using CCmall.Model.Entities;
+using CCmall.Model.Models;
+using System.Collections.Generic;
+
 namespace CCmall.Repository.Interface
 {
     public interface IRouterRepository : IBaseRepository<Router>
     {
+        List<RouterData> GetRouterTree(int parent);
     }
 }
