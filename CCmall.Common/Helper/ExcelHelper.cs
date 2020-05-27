8bc65bb0ba4b9142aea7802b334b381205a2a8f6
Mr`C <635452885@qq.com>
2020-05-28 00:37:06 +0800

    add excel helper

---
+++ CCmall.Common/Helper/ExcelHelper.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Logging;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CCmall.Common.Helper
+{
+    public class ExcelHelper<T>
+    {
+        //Excel sheet最大行数，默认65536
+        public static int sheetSize = 65536;
+        //工作表名称
+        private string sheetName;
+        //导出类型（EXPORT:导出数据；IMPORT：导入模板）
+        private Type type;
+        //工作薄对象
+        private IWorkbook wb;
+        //工作表对象
+        private ISheet sheet;
+        //样式列表
+        private Dictionary<string, ICellStyle> styles;
+        //导入导出数据列表
+        private List<T> list;
+        //注解列表
+        private List<Object[]> fields;
+        public void init(List<T> list, String sheetName, Type type)
+        {
+            if (list == null)
+            {
+                list = new List<T>();
+            }
+            this.list = list;
+            this.sheetName = sheetName;
+            this.type = type;
+            createExcelField();
+            createWorkbook();
+        }
+        /// <summary>
+        /// 得到所有定义字段
+        /// </summary>
+        private void createExcelField()
+        {
+            this.fields = new List<Object[]>();
+            List<T> tempFields = new List<T>();
+            
+            
+        }
+        /// <summary>
+        /// 创建一个工作簿
+        /// </summary>
+        private void createWorkbook()
+        {
+            this.wb = new HSSFWorkbook();
+        }
+    }
+}
