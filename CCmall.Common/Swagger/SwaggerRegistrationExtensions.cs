026bfc20deacd930d108daf7968be61177eec7ac
Mr`C <zhengqin.cao@siger-data.com>
2020-07-28 18:20:31 +0800

    swagger

---
+++ CCmall.Common/Swagger/SwaggerRegistrationExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CCmall.Common.Swagger
+{
+    public static class SwaggerRegistrationExtensions
+    {
+        public static void AddSwaggerService(this IServiceCollection services)
+        {
+            //services.Add
+        }
+    }
+}
