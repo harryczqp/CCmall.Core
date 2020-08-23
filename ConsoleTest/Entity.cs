f0394f37297bb342ff64447f675f64b826fd13ea
Mr`C <635452885@qq.com>
2020-08-23 16:45:11 +0800

    add test

---
+++ ConsoleTest/Entity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleTest.Entity
+{
+    public class Slice
+    {
+        public int Count { get; set; }
+        public bool IsNormal { get; set; } = true;
+    }
+}
---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleTest.Entity;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections.Concurrent;
+using System.Linq;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            var actual = 0;
+            using (var data = new BlockingCollection<Slice>())
+            {
+                Parallel.For(0, 3600, item =>
+            {
+                var num = random.Next(1, 20);
+                Interlocked.Add(ref actual, num);
+                for (int i = 0; i < num; i++)
+                {
+                    data.Add(new Slice { Count = Interlocked.Increment(ref item) });
+                }
+            });
+                data.CompleteAdding();
+            }
+        }
+    }
+}
