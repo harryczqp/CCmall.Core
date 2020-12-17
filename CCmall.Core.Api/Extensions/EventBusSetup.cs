8e47def2523162fb05059ac24ec2901f84eea337
Mr`C <harryczqp@hotmail.com>
2020-12-17 19:10:38 +0800

    add EventBusSetup

---
+++ CCmall.Core.Api/Extensions/EventBusSetup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCmall.Core.Api.Extensions
+{
+    public static class EventBusSetup
+    {
+        public static void AddEventBusSetup(this IServiceCollection services)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            
+        }
+
+
+        public static void ConfigureEventBus(this IApplicationBuilder app)
+        {
+           
+        }
+    }
+}
