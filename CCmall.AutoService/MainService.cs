5d9668c1ea981001a8262e954ff20b4544391659
Mr`C <harryczqp@hotmail.com>
2021-01-14 22:16:33 +0800

    remove some errors

--- CCmall.AutoService/MainService.cs
+++ CCmall.AutoService/MainService.cs
@@ -18,40 +18,40 @@ namespace CCmall.AutoService
         public void start()
         {
             var isService = !(Debugger.IsAttached || args.Contains("--console"));
-            var builder = CreateWebHostBuilder(args.Where(arg => arg != "--console").ToArray());
+            //var builder = CreateWebHostBuilder(args.Where(arg => arg != "--console").ToArray());
 
-            if (isService)
-            {
-                var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
-                var pathToContentRoot = Path.GetDirectoryName(pathToExe);
-                builder.UseContentRoot(pathToContentRoot);
-            }
+            //if (isService)
+            //{
+            //    var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+            //    var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+            //    builder.UseContentRoot(pathToContentRoot);
+            //}
 
-            var host = builder.Build();
-            host.Run();
+            //var host = builder.Build();
+            //host.Run();
         }
         public void stop()
         {
 
         }
 
-        public IHostBuilder CreateWebHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureLogging(logging =>
-                {
-                    logging.ClearProviders();
-                    logging.SetMinimumLevel(LogLevel.Trace);
-                })
-                .UseNLog()
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder
-                        .ConfigureKestrel(options =>
-                        {
-                            options.AllowSynchronousIO = true;
-                        })
-                        .UseStartup<Startup>()
-                        .UseUrls($"http://0.0.0.0:{(args.Length >= 1 ? int.Parse(args[1]) : 5123)}");
-                });
+        //public IHostBuilder CreateWebHostBuilder(string[] args) =>
+        //    Host.CreateDefaultBuilder(args)
+        //        .ConfigureLogging(logging =>
+        //        {
+        //            logging.ClearProviders();
+        //            logging.SetMinimumLevel(LogLevel.Trace);
+        //        })
+        //        .UseNLog()
+        //        .ConfigureWebHostDefaults(webBuilder =>
+        //        {
+        //            webBuilder
+        //                .ConfigureKestrel(options =>
+        //                {
+        //                    options.AllowSynchronousIO = true;
+        //                })
+        //                .UseStartup<Startup>()
+        //                .UseUrls($"http://0.0.0.0:{(args.Length >= 1 ? int.Parse(args[1]) : 5123)}");
+        //        });
     }
 }
